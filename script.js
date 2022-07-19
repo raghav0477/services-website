bebd3e8c031c024826ce72234ee92f86aac3929a
raghav0477 <sharmaraghav0412@gmail.com>
2022-07-19 19:00:57 +0530

    minor changes

--- script.js
+++
@@ -1,7 +0,0 @@
-$(".slick-carousel").slick({
-  infinite: true,
-  slidesToShow: 4, // Shows a three slides at a time
-  slidesToScroll: 1, // When you click an arrow, it scrolls 1 slide at a time
-  arrows: true, // Adds arrows to sides of slider
-  dots: true // Adds the dots on the bottom
-});
